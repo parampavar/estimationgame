a29c8069084f7a782a23b2360abd6db99ee4475a
212307911 <turnerth@ge.com>
2016-01-07 17:20:53 -0800

    Data setup script added

---
+++ db/data_setup.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO users(idp_user_id, name, last_updated, status)
+    VALUES ('100100100', 'User 100', CURRENT_DATE, 'OK');
+INSERT INTO users(idp_user_id, name, last_updated, status)
+    VALUES ('100100101', 'User 101', CURRENT_DATE, 'OK');
+INSERT INTO users(idp_user_id, name, last_updated, status)
+    VALUES ('100100102', 'User 102', CURRENT_DATE, 'OK');
+
+
+INSERT INTO toys(name, "isActive") VALUES ('Water bottle', true);
+INSERT INTO toys(name, "isActive") VALUES ('Milk can', true);
+INSERT INTO toys(name, "isActive") VALUES ('Apple', true);
+INSERT INTO toys(name, "isActive") VALUES ('Banana', true);
+INSERT INTO toys(name, "isActive") VALUES ('TV', true);
+INSERT INTO toys(name, "isActive") VALUES ('Face', true);
+INSERT INTO toys(name, "isActive") VALUES ('Boat', true);
+INSERT INTO toys(name, "isActive") VALUES ('Car', true);
+INSERT INTO toys(name, "isActive") VALUES ('Chair', true);
+INSERT INTO toys(name, "isActive") VALUES ('Table', true);
+INSERT INTO toys(name, "isActive") VALUES ('Couch', true);
+INSERT INTO toys(name, "isActive") VALUES ('Train', true);
+INSERT INTO toys(name, "isActive") VALUES ('Elephant', true);
+INSERT INTO toys(name, "isActive") VALUES ('Airplane', true);
+INSERT INTO toys(name, "isActive") VALUES ('Rocket', true);
+
+INSERT INTO tools(name, "isActive") VALUES ('Modelling Clay', true);
+INSERT INTO tools(name, "isActive") VALUES ('Basic shape stencils', true);
+INSERT INTO tools(name, "isActive") VALUES ('Tape measure', true);
+INSERT INTO tools(name, "isActive") VALUES ('Work space', true);
+INSERT INTO tools(name, "isActive") VALUES ('Studio (to showcase)', true);
